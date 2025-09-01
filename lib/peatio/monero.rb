0ebfb7fe2239dc29485457f92e2b36efde49019d
bradza12 <70218397+bradza12@users.noreply.github.com>
2025-09-01 23:10:42 +0100

    more changes

--- lib/peatio/monero.rb
+++ lib/peatio/monero.rb
@@ -1,10 +1,20 @@
-require 'peatio'
+# frozen_string_literal: true
+
+require "active_support/core_ext/object/blank"
+require "active_support/core_ext/enumerable"
+require "peatio"
 
 module Peatio
   module Monero
-    require 'peatio/monero/blockchain'
-    require 'peatio/monero/client'
-    require 'peatio/monero/wallet'
-    require 'peatio/monero/hooks'
-    require 'peatio/monero/railtie' if defined?(Rails)
-    require 'peatio/monero/version'
+    require "bigdecimal"
+    require "bigdecimal/util"
+
+    require "peatio/monero/blockchain"
+    require "peatio/monero/client"
+    require "peatio/monero/wallet"
+
+    require "peatio/monero/hooks"
+
+    require "peatio/monero/version"
+  end
+end
